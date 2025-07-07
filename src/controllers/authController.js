f68de2207c71b6724a08908effdd602fd949367e
devjaeyoon <devjaeyoon@gmail.com>
2025-07-07 19:02:33 +0900

    feat: implement Logout feature

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,5 +1,23 @@
 import { processKakaoLogin } from '../services/authService.js';
 
+export const handleLogout = (req, res) => {
+  try {
+    res.cookie('accessToken', '', {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: 'strict',
+      path: '/',
+      maxAge: 0,
+    });
+
+    res.status(200).json({ message: '성공적으로 로그아웃되었습니다.' });
+  } catch (error) {
+    console.error('로그아웃 처리 중 에러 발생:', error);
+
+    res.status(500).json({ message: '서버 내부 오류가 발생했습니다.' });
+  }
+};
+
 export const handleKakaoLogin = async (req, res) => {
   try {
     const { code } = req.body;
@@ -13,13 +31,13 @@ export const handleKakaoLogin = async (req, res) => {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
       sameSite: 'strict',
+      path: '/',
       maxAge: 60 * 60 * 1000 * 24,
     });
 
     res.status(200).json({ user });
   } catch (error) {
     console.error('카카오 로그인 처리 중 에러 발생:', error);
-
     res.status(500).json({ message: '서버 내부 오류가 발생했습니다.' });
   }
 };
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,8 +1,10 @@
 import express from 'express';
-import { handleKakaoLogin } from '../controllers/authController.js';
+
+import { handleKakaoLogin, handleLogout } from '../controllers/authController.js';
 
 const router = express.Router();
 
 router.post('/kakao', handleKakaoLogin);
+router.post('/logout', handleLogout);
 
 export default router;
